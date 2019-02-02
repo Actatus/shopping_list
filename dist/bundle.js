bfac0087b6755051e4439633de1b3c041229ae72
Actatus <itsactatus@yahoo.com>
2019-02-02 03:05:35 -0800

    Started scrapper implementation, checks if URL is valid

--- dist/bundle.js
+++ dist/bundle.js
@@ -99497,16 +99497,29 @@ function extend() {
 }
 
 },{}],657:[function(require,module,exports){
-const $ = require('cheerio');
-const rp = require('request-promise');
-
-urlSubmit = function(){
-  console.log(document.getElementById('input_url').value);
-  console.log("click");
-
-};
-
 document.addEventListener("DOMContentLoaded", function(event) {
+
+  const $ = require('cheerio');
+  const rp = require('request-promise');
+  var urlfield = document.getElementById('input_url');
+
+  scrapper = function(targeturl){
+    rp(targeturl)
+      .then(function(){
+        console.log("Is URL");
+      })
+      .catch(function(err){
+        console.log("Not URL");
+      })
+  }
+
+  urlSubmit = function(){
+    console.log(urlfield.value);
+    //if input == valid URL run scraper function.
+    scrapper(urlfield.value);
+    //else throw error.
+  };
+
   document.getElementById('submit_button').addEventListener('click', urlSubmit);
 });
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-const $ = require('cheerio');
-const rp = require('request-promise');
-
-urlSubmit = function(){
-  console.log(document.getElementById('input_url').value);
-  console.log("click");
-
-};
-
 document.addEventListener("DOMContentLoaded", function(event) {
+
+  const $ = require('cheerio');
+  const rp = require('request-promise');
+  var urlfield = document.getElementById('input_url');
+
+  scrapper = function(targeturl){
+    rp(targeturl)
+      .then(function(){
+        console.log("Is URL");
+      })
+      .catch(function(err){
+        console.log("Not URL");
+      })
+  }
+
+  urlSubmit = function(){
+    console.log(urlfield.value);
+    //if input == valid URL run scraper function.
+    scrapper(urlfield.value);
+    //else throw error.
+  };
+
   document.getElementById('submit_button').addEventListener('click', urlSubmit);
 });
